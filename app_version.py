212142f430c7f7cf9bb1aad945dbdc220d16e8cd
hesham98 <h.elamalye2011@gmail.com>
2025-02-05 19:12:28 +0200

    editing pylint warnings

---
+++ app_version.py
@@ -0,0 +1,9 @@
+"""Module providing a function printing the application version."""
+
+VERSION = "0.0.1"
+
+def app_version():
+    """Function printing the application version"""
+    print (VERSION)
+
+app_version()
